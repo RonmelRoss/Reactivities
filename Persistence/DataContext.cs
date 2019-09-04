16820aa5f18dec1096fea9bb2801a6b28b12dd48
Ronmel Ross <ronmel_ross@ligph.com>
2019-09-04 15:10:36 +0800

    003: Creating, seeding the database using Entity Framework.Using Dependency Injection for displaying data from database.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,15 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id=1, Name="Foo Bar"},
+                    new Value {Id=2, Name="Bar Foo"},
+                    new Value {Id=3, Name="Foo Foo"}
+                );
+        }
     }
 }
