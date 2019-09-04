9fb4f469f89912b58b2f3a1ab54563e12953b856
Ronmel Ross <ronmel_ross@ligph.com>
2019-09-04 16:41:02 +0800

    004: Added React client-app for FE

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -43,11 +48,13 @@ namespace API
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseMvc();
+
         }
     }
 }
---
+++ client-app/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+// import logo from './logo.svg';
+import { Header, List } from 'semantic-ui-react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response)=>{
+        this.setState({
+          values:response.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as='h2' icon='users' content='My Project' />
+        <List>
+          {this.state.values.map((val:any)=>(
+            <List.Item key={val.id}>{val.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
